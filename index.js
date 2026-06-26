0d4ae0ad1b26938f87385c4129a047d782e5ee60
pasapasafc-netizen <pasapasafc@gmail.com>
2026-06-26 09:23:20 -0400

    Update index.js

--- index.js
+++ index.js
@@ -14,11 +14,42 @@ client.once('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
 });
 
-client.on('messageCreate', message => {
+client.on('messageCreate', async message => {
   if (message.author.bot) return;
 
+  // Ping
   if (message.content === '!ping') {
-    message.reply('🏆 VFN Betting Bot is online!');
+    return message.reply('🏆 VFN Betting Bot is online!');
+  }
+
+  // Help
+  if (message.content === '!help') {
+    return message.reply(`
+🎮 **VFN BETTING BOT**
+
+Commands:
+
+!ping
+!help
+!balance
+!daily
+!bet
+`);
+  }
+
+  // Balance
+  if (message.content === '!balance') {
+    return message.reply('💰 Your balance: **1000 Coins**');
+  }
+
+  // Daily
+  if (message.content === '!daily') {
+    return message.reply('🎁 You claimed **500 Coins!**');
+  }
+
+  // Bet
+  if (message.content === '!bet') {
+    return message.reply('⚽ Betting system coming soon...');
   }
 });
 
