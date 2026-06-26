4dd757d27fcc67257301b700cb8dbf0fdeda6e4c
pasapasafc-netizen <pasapasafc@gmail.com>
2026-06-26 08:40:13 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ require('dotenv').config();
 const { Client, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds]
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
 });
+  
 
 client.once('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
